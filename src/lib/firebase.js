645552813106eeaa186c14b8522e74982c192e3d
NguyenThuongHuyen <thuonghuyen297@gmail.com>
2022-06-29 23:15:45 +0700

    Save Avatar by Storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyD-tHz353NOL0ddeMUogI9e9znH56zz-_0",
@@ -81,3 +82,26 @@ export const storeUserInfo = async (user) => {
     };
   }
 } 
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+}; 
